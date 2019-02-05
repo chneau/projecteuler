f6b6e94e7bc6588f1af41be690b0e92fe07c6aff
Charles Neau <charles63500@gmail.com>
2019-02-05 12:07:50 +0000

    24

--- problems/24/main.go
+++ problems/24/main.go
@@ -2,8 +2,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"testing"
 )
 
 func factorial(n int) (sum int) {
@@ -14,25 +14,30 @@ func factorial(n int) (sum int) {
 	return
 }
 
-// factorial(n) ... n-1 until 1. modulo
-func a(perm []string, nth int) (sol []string) {
-	x := factorial(len(perm))
-	log.Println(x)
-	copy := append(perm[:0:0], perm...)
-	log.Println(copy)
-	for len(copy) > 0 {
-		i := nth % len(perm)
-		sol = append(sol, copy[i])
-		copy = append(copy[:i], copy[i+1:]...)
-		log.Println(sol)
-		nth /= len(copy)
+func a(perm []string, nth int) (solution []string) {
+	nth--                               // since 1st is 1
+	copy := append(perm[:0:0], perm...) // create a copy we items remove from
+	for len(copy) > 0 {                 // while copy has items
+		facto := factorial(len(copy) - 1)              // factorial of size-1
+		index := nth / facto                           // calculate the index of the item to remove
+		index %= len(copy)                             // modulo size to prevent overflow
+		solution = append(solution, copy[index])       // add to solution and
+		copy = append(copy[:index], copy[index+1:]...) // remove from copy
 	}
-	return perm
+	return
 }
 
 func main() {
 	res := a([]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}, 1e6)
-	fmt.Println("a:", res)
+	log.Println("a:", res)
+	aa := testing.Benchmark(func(b *testing.B) { // ~ 1179 ns/op. pretty good
+		for i := 0; i < b.N; i++ {
+			a([]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}, 1e6)
+		}
+	})
+	println("a:", aa.String(), aa.MemString())
 }
+
+// feels: this one was interesting :)
 
 // Solution is 2783915460
