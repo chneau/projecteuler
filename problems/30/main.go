1f5aa9f86a6ad2b5d5fea97858a647773528e75f
Charles Neau <charles63500@gmail.com>
2019-02-08 15:26:37 +0000

    caching

--- problems/30/main.go
+++ problems/30/main.go
@@ -22,15 +22,43 @@ func a(pow int) (sum int) {
 	return
 }
 
+func b(pow int) (sum int) {
+	tree := map[int]int{} // witch caching
+	for i := 0; i < 10; i++ {
+		tree[i] = int(math.Pow(float64(i), float64(pow)))
+	}
+	for i := 2; i < (pow-1)*int(math.Pow(9, float64(pow))); i++ {
+		n := i
+		nsum := 0
+		for n != 0 {
+			x := n % 10
+			n /= 10
+			nsum += tree[x]
+		}
+		if i == nsum {
+			sum += i
+		}
+	}
+	return
+}
+
 func main() {
 	res := a(5)
 	println("a:", res) // 443839
+	res = b(5)
+	println("b:", res) // 443839
 	aaa := testing.Benchmark(func(bb *testing.B) {
 		for i := 0; i < bb.N; i++ {
 			a(5) // 56ms
 		}
 	})
 	println("a:", aaa.String(), aaa.MemString())
+	aaa = testing.Benchmark(func(bb *testing.B) {
+		for i := 0; i < bb.N; i++ {
+			b(5) // 35ms
+		}
+	})
+	println("b:", aaa.String(), aaa.MemString())
 }
 
 // Solution is 443839
---
+++ problems/31/main.go
@@ -0,0 +1,7 @@
+// https://projecteuler.net/problem=31
+package main
+
+func main() {
+
+}
+// Solution is 73682
