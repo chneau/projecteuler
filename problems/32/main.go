0ccaabce24e5475cb66882943e3f2ac00bb35cda
chneau <charles63500@gmail.com>
2019-08-27 11:53:42 +0100

    32 greedy improv

--- problems/32/main.go
+++ problems/32/main.go
@@ -1,7 +1,10 @@
 // https://projecteuler.net/problem=32
 package main
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 // max = 9 => pandi from 1 to 9
 func pandigital(test, max int) bool {
@@ -34,10 +37,17 @@ func concat(a, b int) int { // not idiot proof, no check if overflow (near above
 
 func a(max int) (sum int) {
 	memory := map[int]struct{}{}
+	tresh := int(math.Pow10(max))
 	for i := 0; i < 1e4; i++ { // how to estime this 1e4 (got it from err and trials)
 		for j := 0; j < 1e4; j++ {
+			if j > i { // halv to 2.5s
+				break
+			}
 			prod := i * j
-			if pandigital(concat(concat(i, j), prod), 9) {
+			if prod > tresh {
+				break
+			}
+			if pandigital(concat(concat(i, j), prod), max) {
 				if _, exist := memory[prod]; exist {
 					continue
 				}
@@ -52,10 +62,9 @@ func a(max int) (sum int) {
 func main() {
 	res := a(9)
 	println("a:", res) // 45228
-
 	aaa := testing.Benchmark(func(bb *testing.B) {
 		for i := 0; i < bb.N; i++ {
-			a(9) // 4.7s
+			a(9) // 2.5s
 		}
 	})
 	println("a:", aaa.String(), aaa.MemString())
